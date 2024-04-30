354fb78e7a5aa7f0fb01c96bfc78cf2a5b842ad0
osachin964 <osachin964@gmail.com>
2024-04-30 15:21:47 +0530

    const added

--- Home.js
+++ Home.js
@@ -1,24 +1,18 @@
 /*Write a Javascript function to validate a login form having two fields: emailId and password.*/
 function validateForm() {
-    var x = document.forms["myForm"]["email"].value;
-    var y = document.forms["myForm"]["pwd"].value;
-   
-    /*Write if-else statement to check if the emailid is user@gamil.com and password is password.*/
-    if (x == "user@gmail.com" && y == "password") {
-        alert("Login Successful");
-        return true;
-    }
-    else {
-        alert("Login Unsuccessful");
-        return false;
-    }
+  var x = document.forms["myForm"]["email"].value;
+  var y = document.forms["myForm"]["pwd"].value;
 
-   
+  /*Write if-else statement to check if the emailid is user@gamil.com and password is password.*/
+  if (x == "user@gmail.com" && y == "password") {
+    alert("Login Successful");
+    return true;
+  } else {
+    alert("Login Unsuccessful");
+    return false;
+  }
 }
 
-var firstName = '';
+const firstName = "";
 
-var firstName = '';
-
-
-
+var firstName = "";
